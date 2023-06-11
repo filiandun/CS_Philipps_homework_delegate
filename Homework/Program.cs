40add6ea391c5a75082c9efac6834643c10e6049
filian <filiandun@gmail.com>
2023-06-11 20:42:20 +0300

    project: 0.1% ready

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -1,42 +1,60 @@
 ﻿namespace Homework
 {
+    public delegate void DriveDelegate();
+
     public abstract class Car
     {
-        protected static ushort distance = 1000; // в метрах
+        public static event DriveDelegate OnDriveDelegate;
 
-        protected ushort maxSpeed; // в км/ч
-        protected ushort acceleration; // в км/с
-        protected char icon;
+        protected static ushort distance = 1000; // общая дистанция в метрах
 
-        public ushort currentDistance = 0;
-        private ushort currentSpeed = 0;
-        private ushort time = 0;
+        protected ushort maxSpeed; // максимальная скорость в км/ч
+        protected ushort acceleration; // ускорение в км/с
+        protected char icon; // иконка
+
+        public ushort currentDistance = 0; // текущее положение на дистанции в м
+        private ushort currentSpeed = 0; // текущая скорост в км/ч
+        private ushort time = 0; // потраченное время
+
         Random random = new Random();
+
+        public void Start()
+        {
+            int readyOrNot = random.Next(0, 2);
+            if (readyOrNot == 0)
+            {
+                this.Drive();
+            }
+            else
+            {
+                Console.WriteLine($"Авто {this.icon} не может участвовать в гонке.");
+            }
+        }
 
         public void Drive()
         {     
-            if (currentDistance < distance)
+            if (this.currentDistance < distance)
             {
-                if (currentSpeed < this.maxSpeed)
+                if (this.currentSpeed < this.maxSpeed)
                 {
-                    currentSpeed += (ushort)random.Next(acceleration - 3, acceleration + 2);
+                    this.currentSpeed += (ushort)random.Next(this.acceleration - 3, this.acceleration + 2);
                 }
 
-                if (currentSpeed >= this.maxSpeed)
+                if (this.currentSpeed >= this.maxSpeed)
                 {
-                    currentSpeed = (ushort)random.Next(maxSpeed - 3, maxSpeed + 8);
+                    this.currentSpeed = (ushort)this.random.Next(this.maxSpeed - 3, this.maxSpeed + 8);
                 }
 
-                currentDistance += (ushort)(currentSpeed * 1000 / 3600);
-                time++;
+                this.currentDistance += (ushort)(this.currentSpeed * 1000 / 3600);
+                this.time++;
             }
 
             Console.Write("START ");
             for (int i = 0; i < 100; ++i)
             {
-                if (i == currentDistance / 10)
+                if (i == this.currentDistance / 10)
                 {
-                    Console.Write(icon);
+                    Console.Write(this.icon);
                 }
                 else
                 {
@@ -45,9 +63,36 @@
             }
             Console.Write(" FINISH");
 
-            Console.Write($" | SPEED: {currentSpeed} km/h");
-            Console.Write($" | DISTANCE: {currentDistance} m");
-            Console.WriteLine($" | TIME: {time} s\n");
+            Console.Write($" | SPEED: {this.currentSpeed} km/h");
+            Console.Write($" | DISTANCE: {this.currentDistance} m");
+            Console.WriteLine($" | TIME: {this.time} s\n");
+        }
+
+        public void Finish()
+        {
+            if (this.currentDistance >= distance)
+            {
+                OnDriveDelegate += this.Finish;
+
+                Console.Write("START ");
+                for (int i = 0; i < 100; ++i)
+                {
+                    if (i == this.currentDistance / 10)
+                    {
+                        Console.Write(this.icon);
+                    }
+                    else
+                    {
+                        Console.Write("_");
+                    }
+                }
+                Console.Write(" FINISH");
+
+                Console.Write($" | MAXSPEED: {this.maxSpeed} km/h");
+                Console.WriteLine($" | TIME: {this.time} s\n");
+            }
+
+            OnDriveDelegate();
         }
     }
 
@@ -94,15 +139,21 @@
                     new TeslaModel3(220, 'T', 30)
                 };
 
+            DriveDelegate driveDelegate = null;
+
+            driveDelegate = cars[0].Drive;
+            driveDelegate += cars[1].Drive;
+            driveDelegate += cars[2].Drive;
+
             while (cars[0].currentDistance < 1000)
             {
                 Thread.Sleep(1000);
                 Console.Clear();
-                foreach (Car car in cars)
-                {
-                    car.Drive();
-                }
+
+                driveDelegate();
             }
+
+            // Ничего мне непонятно, хрень какую-то сделал
         }
     }
 }
